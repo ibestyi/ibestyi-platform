c89173277e19cc196aadda2c43c071df3a30de55
xuxiangke <xuxiangke@greatwall.com.cn>
2019-08-29 15:29:38 +0800

    新增ribbon以及构建包脚本

---
+++ ibestyi-gateway/ibestyi-eureka/script/startup.sh
@@ -0,0 +1,1 @@
+nohup java -Dcom.sun.management.jmxremote.port=48011 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -jar ibestyi-eureka-1.0.jar > nohup.out 2>&1 &
